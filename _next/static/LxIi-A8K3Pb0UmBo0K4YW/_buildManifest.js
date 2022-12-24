e637a474111a2f4d724b8742a74fd138393e5014
Watersilver <35609105+Watersilver@users.noreply.github.com>
2022-12-24 01:53:50 +0000

    Deploying to gh-pages from @ Watersilver/ngp@91f1a34c2173ce97146f03b2afc4fa34b0fc09f2 🚀

--- _next/static/LxIi-A8K3Pb0UmBo0K4YW/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9029ee1ee4dd5727.css","static/chunks/pages/index-3f2e93f67a522232.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
